ab96a7156cc27844eb0aea066feef06edbb85af9
jgaragorry <jgaragorry@gmail.com>
2025-10-21 12:38:10 -0300

    se anexo un par d elineas en el modulo notification que refieren al coreo y la forma de notificacion

--- terraform/main.tf
+++ terraform/main.tf
@@ -144,6 +144,8 @@ resource "aws_budgets_budget" "monthly_budget" {
     threshold           = 80
     threshold_type      = "PERCENTAGE"
     notification_type   = "ACTUAL"
+    subscriber_email_addresses = ["jgaragorry@gmail.com"]
+    subscriber_sns_topic_arns  = []
 
     subscriber {
       subscription_type = "EMAIL"
