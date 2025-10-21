e0d85eb2dbbc9a738f04b1d27caf24226e234e3e
jgaragorry <jgaragorry@gmail.com>
2025-10-21 11:58:12 -0300

    actualizacion del main.tf outpust.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,5 +1,25 @@
 output "site_url" {
-  description = "URL pública del sitio web"
+  description = "URL del sitio desplegado en CloudFront"
   value       = aws_cloudfront_distribution.cdn.domain_name
+}
+
+output "bucket_name" {
+  description = "Nombre del bucket S3 creado para el sitio"
+  value       = aws_s3_bucket.website_bucket.id
+}
+
+output "budget_name" {
+  description = "Nombre del presupuesto FinOps creado"
+  value       = aws_budgets_budget.monthly_budget.name
+}
+
+output "headers_policy_id" {
+  description = "ID de la política de headers aplicada en CloudFront"
+  value       = aws_cloudfront_response_headers_policy.security_headers.id
+}
+
+output "origin_access_control_id" {
+  description = "ID del control de acceso de origen para CloudFront"
+  value       = aws_cloudfront_origin_access_control.oac.id
 }
 
