e93b50df30c43e2478ec0920998711949a58638a
jgaragorry <jgaragorry@gmail.com>
2025-10-21 12:41:01 -0300

    corrigio el formto de la session de notificaction

--- terraform/main.tf
+++ terraform/main.tf
@@ -140,17 +140,12 @@ resource "aws_budgets_budget" "monthly_budget" {
   time_period_start = formatdate("YYYY-MM-DD_HH:mm", timestamp())
 
   notification {
-    comparison_operator = "GREATER_THAN"
-    threshold           = 80
-    threshold_type      = "PERCENTAGE"
-    notification_type   = "ACTUAL"
-    subscriber_email_addresses = ["jgaragorry@gmail.com"]
-    subscriber_sns_topic_arns  = []
-
-    subscriber {
-      subscription_type = "EMAIL"
-      address           = "jgaragorry@gmail.com"
-    }
+    comparison_operator          = "GREATER_THAN"
+    threshold                    = 80
+    threshold_type               = "PERCENTAGE"
+    notification_type            = "ACTUAL"
+    subscriber_email_addresses   = ["tu-correo@ejemplo.com"]
+    subscriber_sns_topic_arns    = []
   }
 }
 
